073b17af5445a2e7601764d44c49679deb8362ee
programmerdhruv <programmerdhruv@gmail.com>
2024-01-02 12:43:16 +0530

    init

---
+++ src/test/java/com/matrimonial/WebMatrimonialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrimonial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMatrimonialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
